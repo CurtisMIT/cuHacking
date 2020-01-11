105514242bd34b183810fa23637a4cfab24547b0
CurtisMIT <curtismitmit@gmail.com>
2020-01-11 15:43:06 -0500

    blueprint views

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,9 +1,17 @@
 import os
 from flask import Flask, jsonify, request, abort, make_response
 from . import main
+import json
+
 
 @main.route("/", methods=["GET"])
 def index():
     return 'hello world'
 
+@main.route("/clean", methods=["GET"])
+def clean():
+    f = open('Murder-on-the-2nd-Floor-Raw-Data.json')
+    data = json.load(f)
+    f.close()
 
+    return data
